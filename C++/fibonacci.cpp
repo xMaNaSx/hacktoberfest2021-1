9ce0ced8cc6ded673ffa8fb567db629fe809685a
MeetVaishnav23 <79635039+MeetVaishnav23@users.noreply.github.com>
2021-10-04 08:13:00 +0530

    Update fibonacci.cpp

--- C++/fibonacci.cpp
+++ C++/fibonacci.cpp
@@ -1,29 +1,22 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    int n, t1 = 0, t2 = 1, nextTerm = 0;
-
-    cout << "Enter the number of terms: ";
-    cin >> n;
-
-    cout << "Fibonacci Series: ";
-
-    for (int i = 1; i <= n; ++i) {
-        // Prints the first two terms.
-        if(i == 1) {
-            cout << t1 << ", ";
-            continue;
-        }
-        if(i == 2) {
-            cout << t2 << ", ";
-            continue;
-        }
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-        
-        cout << nextTerm << ", ";
-    }
-    return 0;
+int main()
+{
+	int n,a,b,next,i;
+	
+	cout<<"Enter Number : ";
+	cin>>n;
+	
+	a = 0;
+	b = 1; 
+	next = 0;
+	
+	for(i=1; i<=n; i++)
+	{
+		cout<<next<<" ";
+		a = b;
+		b = next;
+		next = a+b;
+	}
+	
 }
